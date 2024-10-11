8357c352c12ba1911ad119ceda2d8add3c0c01b7
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-11 14:54:09 +0700

    feat: add utils and dummies

--- controllers/post.go
+++ controllers/post.go
@@ -1,1 +1,16 @@
 package controllers
+
+type Post struct {
+	ID        int          `json:"id"`
+	Title     string       `json:"title"`
+	Body      string       `json:"body"`
+	Tags      []string     `json:"tags"`
+	Reactions PostReaction `json:"reactions"`
+	Views     int          `json:"views"`
+	UserId    int          `json:"userId"`
+}
+
+type PostReaction struct {
+	Likes   int `json:"likes"`
+	Disikes int `json:"dislikes"`
+}

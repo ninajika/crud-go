68c72017e0e930c64054e83a1457bfaa718ed1e2
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-11 22:51:36 +0700

    feat: move struct to PostType

--- models/post.go
+++ models/post.go
@@ -8,3 +8,18 @@ type Post struct {
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type PostType struct {
+	ID        int          `json:"id"`
+	Title     string       `json:"title"`
+	Body      string       `json:"body"`
+	Tags      []string     `json:"tags"`
+	Reactions PostReaction `json:"reactions"`
+	Views     int          `json:"views"`
+	UserId    int          `json:"userId"`
+}
+
+type PostReaction struct {
+	Likes   int `json:"likes"`
+	Disikes int `json:"dislikes"`
+}

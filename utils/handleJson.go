148f352f4857722253c264c08abb7f900ae25d1d
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-11 16:02:11 +0700

    feat: add GetJson and ReadJson

---
+++ utils/handleJson.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ninajika/crud-go/controllers"
+)
+
+func GetJson(id string) (*controllers.PostType, error) {
+	result, err := ReadJson[controllers.PostType](fmt.Sprintf("dummies/%s/post.json", id))
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return result, nil
+}
+
+func ReadJson[R any](path string) (*R, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer jsonFile.Close()
+	bytesValue, _ := io.ReadAll(jsonFile)
+	var result *R
+	json.Unmarshal(bytesValue, &result)
+	return result, nil
+}

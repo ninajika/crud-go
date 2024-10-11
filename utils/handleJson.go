66f0cf29cc024561b57f51f01944fa27b59a53c9
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-11 21:22:11 +0700

    feat: add function for crud

--- utils/handleJson.go
+++ utils/handleJson.go
@@ -4,19 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
-
-	"github.com/ninajika/crud-go/controllers"
+	"path/filepath"
 )
 
-func GetDump(id string) (*controllers.PostType, error) {
-	result, err := ReadJson[controllers.PostType](fmt.Sprintf("dummies/%s/post.json", id))
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return result, nil
-}
+// func GetDump(id string) (*controllers.PostType, error) {
+// 	result, err := ReadJson[controllers.PostType](fmt.Sprintf("dummies/%s/post.json", id))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil, err
+// 	}
+// 	return result, nil
+// }
 
 func ReadJson[R any](path string) (*R, error) {
 	jsonFile, err := os.Open(path)
@@ -30,4 +30,57 @@ func ReadJson[R any](path string) (*R, error) {
 	var result *R
 	json.Unmarshal(bytesValue, &result)
 	return result, nil
+}
+
+func CreateJson[R any](id string, data *R) error {
+	dirPath := fmt.Sprintf("dummies/%s", id)
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return err
+	}
+
+	filePath := filepath.Join(dirPath, "post.json")
+	if _, err := os.Stat(filePath); err == nil {
+		return fmt.Errorf("post.json already exists for ID: %s", id)
+	}
+
+	return writeJson(filePath, data)
+}
+
+func UpdateJson[R any](id string, data *R) error {
+	dirPath := fmt.Sprintf("dummies/%s", id)
+	filePath := filepath.Join(dirPath, "post.json")
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return fmt.Errorf("post.json does not exist for ID: %s", id)
+	}
+
+	return writeJson(filePath, data)
+}
+
+func writeJson[R any](filePath string, data *R) error {
+	jsonValue, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return err
+	}
+
+	err = os.WriteFile(filePath, jsonValue, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return err
+	}
+
+	return nil
+}
+
+func DeletePost(id string) error {
+	log.Printf("Deleting post with ID: %s", id)
+	err := os.Remove(fmt.Sprintf("dummies/%s/post.json", id))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }

72574db0e907396cdf936464458c2f73f42e1d49
ninajika <32153003+ninajika@users.noreply.github.com>
2024-10-11 22:51:02 +0700

    fix: fix some error

--- controllers/post.go
+++ controllers/post.go
@@ -4,34 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ninajika/crud-go/models"
 	"github.com/ninajika/crud-go/utils"
 )
-
-type PostType struct {
-	ID        int          `json:"id"`
-	Title     string       `json:"title"`
-	Body      string       `json:"body"`
-	Tags      []string     `json:"tags"`
-	Reactions PostReaction `json:"reactions"`
-	Views     int          `json:"views"`
-	UserId    int          `json:"userId"`
-}
-
-type PostReaction struct {
-	Likes   int `json:"likes"`
-	Disikes int `json:"dislikes"`
-}
 
 func GetPostById(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Println(id)
-	result, err := utils.ReadJson[PostType](fmt.Sprintf("dummies/%s/post.json", id))
+	result, err := utils.GetDump(id)
 	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, result)
@@ -39,27 +23,13 @@ func GetPostById(c *gin.Context) {
 
 func CreatePostById(c *gin.Context) {
 	id := c.Param("id")
-	var data PostType
+	var data models.PostType
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	dirPath := fmt.Sprintf("dummies/%s", id)
-
-	err := os.MkdirAll(dirPath, os.ModePerm)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-
-	filePath := filepath.Join(dirPath, "post.json")
-	if _, err := os.Stat(filePath); err == nil {
-		c.JSON(400, gin.H{"error": fmt.Sprintf("post.json already exists for ID: %s", id)})
-		return
-	}
-
-	if err := utils.WriteJson(filePath, &data); err != nil {
+	if err := utils.CreateJson(id, &data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -69,21 +39,13 @@ func CreatePostById(c *gin.Context) {
 
 func UpdatePostById(c *gin.Context) {
 	id := c.Param("id")
-	var data PostType
+	var data models.PostType
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	dirPath := fmt.Sprintf("dummies/%s", id)
-	filePath := filepath.Join(dirPath, "post.json")
-
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		c.JSON(400, gin.H{"error": fmt.Sprintf("post.json does not exist for ID: %s", id)})
-		return
-	}
-
-	if err := utils.WriteJson(filePath, &data); err != nil {
+	if err := utils.UpdateJson(id, &data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -94,9 +56,9 @@ func UpdatePostById(c *gin.Context) {
 func RemovePostById(c *gin.Context) {
 	id := c.Param("id")
 	log.Printf("Deleting post with ID: %s", id)
-	err := os.Remove(fmt.Sprintf("dummies/%s/post.json", id))
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	if err := utils.DeletePost(id); err != nil {
+		log.Println(err)
+		c.JSON(404, gin.H{"error": "Not found"})
 		return
 	}
 	c.Status(204)
